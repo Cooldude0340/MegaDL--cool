048cdc83aa688dfb0fb1a79e5252a2c0ff4a1681
CD-MCV <84226164+Cooldude0340@users.noreply.github.com>
2024-06-29 17:10:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     TG_MAX_SIZE = 2040108421
     OWNER_ID = int(os.environ.get("OWNER_ID", 1517181772))
-    LOG_CHANNEL = int(os.environ.get("1210713881"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1713544941"))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 
 
